0af76498f626c26bcd633ed746fa427f82d6ed72
Niwiar-Priva <87194989+Niwiar@users.noreply.github.com>
2023-07-27 11:01:38 +0700

    rebuild

--- src/libs/socket-io.ts
+++ src/libs/socket-io.ts
@@ -2,14 +2,6 @@ import { Server, Socket } from 'socket.io';
 import http from 'http';
 
 let io: Server;
-
-interface Character {
-  id: string;
-  name: string;
-  health: number;
-  stamina: number;
-}
-
 export const socketServer = (server: http.Server) => {
   io = new Server(server, {
     cors: {
