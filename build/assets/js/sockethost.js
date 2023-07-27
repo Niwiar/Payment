d1e07d9cd70bb01c8ab95eaa6a16d5786e768d2d
Niwiar-Priva <87194989+Niwiar@users.noreply.github.com>
2023-07-27 11:22:09 +0700

    update socket host

--- build/assets/js/sockethost.js
+++ build/assets/js/sockethost.js
@@ -1,3 +1,3 @@
 let hostname = window.location.hostname;
-if (window.location.protocol === 'http') hostname + ':3000';
+if (window.location.protocol === 'http') hostname += ':3000';
 const socketHost = window.location.protocol + '//' + hostname;
